07783f4e191aa1f1a241d9772fe91c55370cbaa2
dqdzung <dinhquangdung1011@gmail.com>
2023-05-18 15:21:46 +0700

    flip card achievement

--- src/lib/game/achievementItem.svelte
+++ src/lib/game/achievementItem.svelte
@@ -3,34 +3,67 @@
 	import { IconCheckbox } from '@tabler/icons-svelte';
 
 	export let data: any;
+	export let showCardBack: boolean;
 
 	let screenSize: number;
 </script>
 
 <svelte:window bind:innerWidth={screenSize} />
 
-<Card
-	shadow="sm"
-	padding="lg"
-	radius="md"
-	override={{
-		background: data.achieved === 0 ? '#191919' : 'rgb(60, 60, 60)',
-		color: 'white'
-	}}
->
-	<Group position="apart">
-		<div>
+{#if !showCardBack}
+	<Card
+		shadow="sm"
+		padding="lg"
+		radius="md"
+		override={{
+			display: 'flex',
+			alignItems: 'center',
+			minHeight: '65px',
+			background: data.achieved === 0 ? '#191919' : 'rgb(60, 60, 60)',
+			color: 'white',
+			'&:hover': {
+				background: 'rgb(90, 90, 90)'
+			}
+		}}
+	>
+		<Group position="apart" override={{ width: '100%' }}>
 			{data.name}
-		</div>
 
-		{#if screenSize >= 900}
-			{data.description}
-		{/if}
-
-		<div>
 			{#if data.achieved}
 				<IconCheckbox />
 			{/if}
+		</Group>
+	</Card>
+{:else}
+	<Card
+		shadow="sm"
+		padding="lg"
+		radius="md"
+		override={{
+			justifyContent: 'center',
+			display: 'flex',
+			alignItems: 'center',
+			minHeight: '65px',
+			transform: 'rotateY(180deg)',
+			color: 'black',
+			'&:hover': {
+				background: 'rgb(220, 220, 220)'
+			}
+		}}
+	>
+		<div style="opacity: 0" class:conceal-answer={showCardBack}>
+			{data.description}
 		</div>
-	</Group>
-</Card>
+	</Card>
+{/if}
+
+<style>
+	@keyframes revealTextSlowly {
+		to {
+			opacity: 1;
+		}
+	}
+	.conceal-answer {
+		animation: revealTextSlowly 0.5s forwards;
+	}
+</style>
--- src/lib/game/userStatTab.svelte
+++ src/lib/game/userStatTab.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { ActionIcon, Box, Card, Group, Progress, Stack, Text, Tooltip } from '@svelteuidev/core';
+	import { ActionIcon, Box, Card, Progress, Stack, Text, Tooltip } from '@svelteuidev/core';
 	import { calculateCheevoCompletion } from '../../routes/game/[appid]/utils';
 	import { IconChevronDown, IconChevronUp } from '@tabler/icons-svelte';
 	import { slide } from 'svelte/transition';
@@ -8,6 +8,17 @@
 	export let data: any;
 	let openCheevo = false;
 	let screenSize: number;
+	let showCardBack: number[] = [];
+
+	const toggleShowBack = (index: number) => {
+		if (showCardBack.includes(index)) {
+			showCardBack = showCardBack.filter((item) => item !== index);
+		} else {
+			showCardBack = [...showCardBack, index];
+		}
+
+		console.log(showCardBack);
+	};
 
 	const onOpenCheevo = () => {
 		openCheevo = !openCheevo;
@@ -30,15 +41,14 @@
 
 	{#if openCheevo}
 		<div transition:slide style="margin-top: 20px;">
-			<Stack spacing={10}>
-				{#each data as cheevo}
-					{#if screenSize > 900}
-						<AchievementItem data={cheevo} />
-					{:else}
-						<Tooltip label={cheevo.description} withArrow arrowSize={5} withinPortal wrapLines color='blue'>
-							<AchievementItem data={cheevo} />
-						</Tooltip>
-					{/if}
+			<Stack spacing={20}>
+				{#each data as cheevo, i}
+					<!-- svelte-ignore a11y-click-events-have-key-events -->
+					<div class="flip-card" on:click={() => toggleShowBack(i)}>
+						<div class="flip-card-inner" class:flip-it={showCardBack.includes(i)}>
+							<AchievementItem showCardBack={showCardBack.some((e) => e === i)} data={cheevo} />
+						</div>
+					</div>
 				{/each}
 			</Stack>
 		</div>
@@ -60,5 +70,29 @@
 <style lang="scss">
 	* {
 		color: white;
+	}
+
+	/* The flip box container - set the width and height to whatever you want. We have added the border property to demonstrate that the flip itself goes out of the box on hover (remove perspective if you don't want the 3D effect */
+	.flip-card {
+		background-color: transparent;
+		width: 100%;
+		height: 100%;
+		perspective: 2000px;
+		cursor: pointer;
+	}
+
+	/* This container is needed to position the front and back side */
+	.flip-card-inner {
+		position: relative;
+		width: 100%;
+		height: 100%;
+		text-align: center;
+		transition: transform 0.4s;
+		transform-style: preserve-3d;
+	}
+
+	/* Do an horizontal flip on button click */
+	.flip-it {
+		transform: rotateY(180deg);
 	}
 </style>
